cea7cde2e1a779a13a32041d760d21a681537af8
princebillygk <princebillygk@gmail.com>
2025-01-13 23:24:18 +0600

    feat(nvim-tree): added shortcut ? and c-t

--- init.lua
+++ init.lua
@@ -27,6 +27,26 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
+
+local function my_on_attach(bufnr)
+local api = require "nvim-tree.api"
+
+local function opts(desc)
+  return { 
+    desc = "nvim-tree: " .. desc,
+    buffer = bufnr,
+    noremap = true,
+    silent = true,
+    nowait = true 
+  }
+end
+
+api.config.mappings.default_on_attach(bufnr)
+
+vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
+
 require("nvim-tree").setup({
   on_attach = my_on_attach,
   sort = {
